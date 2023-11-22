a9aaa73df8bd0d3e66e013b25033a0967924a426
Rok Šekoranja <rok.sekoranja@priot.io>
2023-11-23 00:20:36 +0100

    Added json for vercel deployment

--- awesome-weather-app/src/app/current-weather/current-weather.component.ts
+++ awesome-weather-app/src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { WeatherService } from '../weather.service';
 
 @Component({
@@ -10,13 +9,29 @@ import { WeatherService } from '../weather.service';
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.scss',
 })
-export class CurrentWeatherComponent implements OnInit {
+export class CurrentWeatherComponent implements OnInit, OnDestroy {
   currentWeatherData: any;
+  private intervalId: any;
 
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.fetchCurrentWeather();
+    this.startInterval();
+  }
+
+  ngOnDestroy(): void {
+    this.stopInterval();
+  }
+
+  startInterval(): void {
+    this.intervalId = setInterval(() => {
+      this.fetchCurrentWeather();
+    }, 5000);
+  }
+
+  stopInterval(): void {
+    clearInterval(this.intervalId);
   }
 
   fetchCurrentWeather() {
